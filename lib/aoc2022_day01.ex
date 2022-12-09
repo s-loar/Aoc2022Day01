eb18ff89ac6cebc5358ed9055d5a2f03db60c7ed
s-loar <sjl7909@gmail.com>
2022-12-09 16:20:44 -0500

    Part 2 - the sum of the top 3 calories

--- lib/aoc2022_day01.ex
+++ lib/aoc2022_day01.ex
@@ -7,6 +7,15 @@ defmodule Aoc2022Day01 do
     |> Enum.max()
   end
 
+  def top_three_sum do
+    File.read!('./input.txt')
+    |> String.split( "\n\n")
+    |> Enum.map(&sum_calories/1)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
+  end
+
   defp sum_calories(calories) do
     calories
     |> String.split("\n", trim: true)
