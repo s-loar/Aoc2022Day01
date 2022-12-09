503f3c6886db6a6e2e21d4c082dbe1ff3ffa1761
s-loar <sjl7909@gmail.com>
2022-12-09 14:52:33 -0500

    first commit

---
+++ lib/aoc2022_day01.ex
@@ -0,0 +1,16 @@
+defmodule Aoc2022Day01 do
+
+  def max_calories do
+    File.read!('./input.txt')
+    |> String.split( "\n\n")
+    |> Enum.map(&sum_calories/1)
+    |> Enum.max()
+  end
+
+  defp sum_calories(s) do
+    s
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.sum()
+  end
+end
---
+++ test/aoc2022_day01_test.exs
@@ -0,0 +1,8 @@
+defmodule Aoc2022Day01Test do
+  use ExUnit.Case
+  doctest Aoc2022Day01
+
+  test "greets the world" do
+    assert Aoc2022Day01.hello() == :world
+  end
+end
